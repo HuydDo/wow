9480684cacfb6c571658c36bbe12c0ae974550a1
HuydDo <huydinhdo@yahoo.com>
2020-04-17 12:41:22 -0600

    set up index.js and created our store

--- wow-frontend/src/index.js
+++ wow-frontend/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+let reducer =''
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE_  || compose
+let store = createStore(reducer, 
+  composeEnhancers (applyMiddleware(thunk)))
+
+// const store = createStore(catsReducer, 
+//   compose (applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
 
 
