3ef308a88190a8ce8616940a343a634858c056e6
HuydDo <huydinhdo@yahoo.com>
2020-04-17 00:38:25 -0600

    update App.js and index.js

--- wow-frontend/src/App.js
+++ wow-frontend/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
 
+const blizzard = require('blizzard.js').initialize({
+  key: process.env.BLIZZARD_CLIENT_ID,
+  secret: process.env.BLIZZARD_CLIENT_SECRET,
+  origin: 'us',
+  locale: 'en_US'
+});
+
 class App extends React.Component {
+   w = async function example () {
+    try {
+      await blizzard.getApplicationToken()
+        .then(response => {
+          blizzard.defaults.token = response.data.access_token
+        });
+      const item = await blizzard.wow.item({ id: 168185 });
+      console.log(item)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+    
+  
+
   render(){
     return (
       <div className="App">
-        
+        {this.props.w}
       </div>
     );
   }
--- wow-frontend/src/index.js
+++ wow-frontend/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h2>World of Warcraft</h2>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
