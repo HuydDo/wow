6ef7a886424a6957dedee9a23c7d1a459741a6d0
HuydDo <huydinhdo@yahoo.com>
2020-04-23 10:19:19 -0600

    modified App.css, About.js, NavBar.js

--- wow-frontend/src/components/About.js
+++ wow-frontend/src/components/About.js
@@ -3,9 +3,9 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <p>This App will allow the user to retrieve or create a new player.  
+      <p>This App will let user to retrieve or create a new player.  
         User has an option to delete or edit their player.  
-        Also, user can create and delete character who belong to the player.
+        Also, user can create and delete character who is belong to the player.
       </p>
     </div>
   );
--- wow-frontend/src/components/NavBar.js
+++ wow-frontend/src/components/NavBar.js
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <ul>
-       <li><NavLink to="/" exact>Home</NavLink></li>
-       <li><NavLink to="/About" exact>About</NavLink></li>
-       <li><NavLink to="/players" exact>Players</NavLink></li>
-       <li><NavLink to="/players/new" exact>Create new player</NavLink></li>
+    <div>
+      <ul class="topnav" >
+       <li className="navbar"><NavLink to="/" exact>Home</NavLink></li>
+       <li className="navbar"><NavLink to="/About" exact>About</NavLink></li>
+       <li className="navbar"><NavLink to="/players" exact>Players</NavLink></li>
+       <li className="navbar"><NavLink to="/players/new" exact>Create new player</NavLink></li>
       </ul>
     </div>
   );
