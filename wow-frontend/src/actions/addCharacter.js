be44ef53f79f8d4ab6d327ce5a88a625a9318b35
HuydDo <huydinhdo@yahoo.com>
2020-07-01 17:55:50 -0600

    modified Procfile, client package.json, addCharacters.js

--- wow-frontend/src/actions/addCharacter.js
+++ wow-frontend/src/actions/addCharacter.js
@@ -1,7 +1,11 @@
+const apiURL = 'http://localhost:3000'
 export const addCharacter = (character, playerId) => {
   
   return(dispatch) => {
-    fetch(`http://localhost:3000/api/v1/players/${playerId}/characters`, {
+    // fetch(`http://localhost:3000/api/v1/players/${playerId}/characters`, {
+    fetch(`${apiURL}/api/v1/players/${playerId}/characters`, {
+    // fetch(`/api/v1/players/${playerId}/characters`, {
+
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
